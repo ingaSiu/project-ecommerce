1d907213c0672a3a8439d1b9d533279ffe8f8bc5
ingaSiu <ingasiud@gmail.com>
2024-04-15 07:52:14 +0300

    add delete and toggle actions to dropdown menu

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { deleteProduct, toggleProductAvailability } from '../../_actions/products';
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { useTransition } from 'react';
+
+export function ActiveToggleDropdownItem({
+  id,
+  isAvailableForPurchase,
+}: {
+  id: string;
+  isAvailableForPurchase: boolean;
+}) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <DropdownMenuItem
+      disabled={isPending}
+      onClick={() => {
+        startTransition;
+        async () => {
+          await toggleProductAvailability(id, !isAvailableForPurchase);
+        };
+      }}
+    >
+      {isAvailableForPurchase ? 'Deactivate' : 'Activate'}
+    </DropdownMenuItem>
+  );
+}
+
+export function DeleteDropdownItem({ id, disabled }: { id: string; disabled: boolean }) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <DropdownMenuItem
+      disabled={disabled || isPending}
+      onClick={() => {
+        startTransition;
+        async () => {
+          await deleteProduct(id);
+        };
+      }}
+    >
+      Delete
+    </DropdownMenuItem>
+  );
+}
