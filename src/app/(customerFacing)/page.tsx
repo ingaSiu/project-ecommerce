5c020f67f36b07aa120f8d46076b30cf5e9fb179
ingaSiu <ingasiud@gmail.com>
2024-04-18 15:46:57 +0300

    fetch products, add product cards grid

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,57 @@
+import { ArrowRight } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { Product } from '@prisma/client';
+import { ProductCard } from '@/components/ProductCard';
+import db from '@/db/db';
+
+function getMostPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: 'desc' } },
+    take: 6,
+  });
+}
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: 'desc' },
+    take: 6,
+  });
+}
+
 export default function HomePage() {
-  return <h1>Hi</h1>;
+  return (
+    <main className="space-y-12">
+      <ProductGridSection productsFetcher={getMostPopularProducts} title="Most popular" />
+      <ProductGridSection productsFetcher={getNewestProducts} title="Newest" />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  productsFetcher: () => Promise<Product[]>;
+  title: string;
+};
+
+async function ProductGridSection({ productsFetcher, title }: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {(await productsFetcher()).map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
 }
