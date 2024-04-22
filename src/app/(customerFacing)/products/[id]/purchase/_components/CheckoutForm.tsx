b1ded607001f4b3b08eb227985c544bc500f8512
ingaSiu <ingasiud@gmail.com>
2024-04-22 11:23:29 +0300

    add error msg, loading state, start confirmation

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Elements, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { FormEvent, useEffect, useState } from 'react';
 
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { formatCurrency } from '@/lib/formatters';
 import { loadStripe } from '@stripe/stripe-js';
-import { useEffect } from 'react';
 
 type CheckoutFormProps = {
   product: {
@@ -35,15 +37,59 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
         </div>
       </div>
       <Elements options={{ clientSecret }} stripe={stripePromise}>
-        <Form />
+        <Form priceInCents={product.priceInCents} />
       </Elements>
     </div>
   );
 }
 
-function Form() {
+function Form({ priceInCents }: { priceInCents: number }) {
   const stripe = useStripe();
   const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
 
-  return <PaymentElement />;
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (stripe == null || elements == null) return;
+
+    setIsLoading(true);
+
+    //check for existing order
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === 'card_error' || error.type === 'validation_error') {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage('An unkwons error occured');
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && <CardDescription className="text-destructive">{errorMessage}</CardDescription>}
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" size="lg" disabled={stripe == null || elements == null || isLoading}>
+            {isLoading ? 'Purchasing...' : ` Purchase - ${formatCurrency(priceInCents / 100)}`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
