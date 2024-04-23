6863953c3a7731470daa22599979028aad58b92f
ingaSiu <ingasiud@gmail.com>
2024-04-23 12:50:33 +0300

    check if null, add success msg

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,14 +1,24 @@
 import Image from 'next/image';
 import Stripe from 'stripe';
+import db from '@/db/db';
 import { formatCurrency } from '@/lib/formatters';
+import { notFound } from 'next/navigation';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function SuccessPage({ searchParams }: { searchParams: { payment_intent: string } }) {
   const paymentIntent = await stripe.paymentIntents.retrieve(searchParams.payment_intent);
 
+  if (paymentIntent == null) return notFound();
+
+  const product = await db.product.findUnique({ where: { id: paymentIntent.metadata.productId } });
+  if (product == null) return notFound();
+
+  const isSuccess = paymentIntent.status === 'succeeded';
+
   return (
     <div className="max-w-5xl w-full mx-auto space-y-8">
+      <h1 className="text-4xl font-bold">{isSuccess ? 'Success!' : 'Error!'}</h1>
       <div className="flex gap-4 items-center">
         <div className="aspect-video flex-shrink-0 w-1/3 relative">
           <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
